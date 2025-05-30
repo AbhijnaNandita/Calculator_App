d2517d99a37ab1fc6832ad849496a6ce939ff6e4
AbhijnaNandita <abhijna07udupa@gmail.com>
2025-05-30 12:48:07 +0530

    Update Calculator.jsx

--- Calculator/src/Components/Calculator.jsx
+++ Calculator/src/Components/Calculator.jsx
@@ -1,36 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Calculator = () => {
+
+  const [data, setData] = useState(" ");
+
+  const getValue = (event) => {
+      setData(data.concat(event.target.value));
+  }
+
+  const calculation = () => {
+    setData(eval(data));
+  }
+
   return (
     <>
       <div className='container'>
-        <input placeholder='0'/>
+        <input placeholder='0' value={data}/>
       
 
-      <button value='('>(</button>
-      <button value=')'>)</button>
-      <button value='%'>%</button>
+      <button onClick={getValue} value='('>(</button>
+      <button onClick={getValue} value=')'>)</button>
+      <button onClick={getValue} value='%'>%</button>
       <button valu='AC'>AC</button>
 
-      <button value='7'>7</button>
-      <button value='8'>8</button>
-      <button value='9'>9</button>
-      <button value='*'>*</button>
+      <button onClick={getValue} value='7'>7</button>
+      <button onClick={getValue} value='8'>8</button>
+      <button onClick={getValue} value='9'>9</button>
+      <button onClick={getValue} value='*'>*</button>
 
-      <button value='4'>4</button>
-      <button value='5'>5</button>
-      <button value='6'>6</button>
-      <button value='-'>-</button>
+      <button onClick={getValue} value='4'>4</button>
+      <button onClick={getValue} value='5'>5</button>
+      <button onClick={getValue} value='6'>6</button>
+      <button onClick={getValue} value='-'>-</button>
 
-      <button value='1'>1</button>
-      <button value='2'>2</button>
-      <button value='3'>3</button>
-      <button value='+'>+</button>
+      <button onClick={getValue} value='1'>1</button>
+      <button onClick={getValue} value='2'>2</button>
+      <button onClick={getValue} value='3'>3</button>
+      <button onClick={getValue} value='+'>+</button>
 
-      <button value='0'>0</button>
-      <button value='BACK'>BACK</button>
-      <button value='='>=</button>
-      <button value='/'>/</button>
+      <button onClick={getValue} value='0'>0</button>
+      <button onClick={getValue} value='BACK'>BACK</button>
+      <button onClick={calculation} value='='>=</button>
+      <button onClick={getValue} value='/'>/</button>
 
       </div>
 
